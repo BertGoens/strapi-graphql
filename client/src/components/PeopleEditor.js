a94151edfc0521e1a10c10f9713198ddb21f5ab3
Bert Goens <bertgoens@gmail.com>
2019-07-28 22:45:17 +0200

    not working with <Query>

--- client/src/components/PeopleEditor.js
+++ client/src/components/PeopleEditor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Mutation } from "react-apollo";
-import { UPDATE_PROFILE } from "../queries/profile";
+import { Mutation, Query } from "react-apollo";
+import { GET_PROFILE, UPDATE_PROFILE } from "../queries/profile";
 
 export function PeopleEditor({ profile }) {
   const [firstNameField, setFirstNameField] = useState("");
@@ -12,7 +12,7 @@ export function PeopleEditor({ profile }) {
     setLastNameField(lastName);
   }, [firstName, lastName, profile]);
 
-  const _updateAvatar = async e => {
+  const _updateAvatar = (data, client, refetch) => async e => {
     const file = document.getElementById("fileItem").files[0];
     if (!file) {
       console.warn("No file selected");
@@ -40,13 +40,33 @@ export function PeopleEditor({ profile }) {
       body: formData
     };
 
+    // upload the avatar
     const response = await fetch(url, requestInit);
+
+    // update apollo cache
     if (response.ok) {
       const parsed = await response.json();
+      // select our file details
       const uploadDetails = parsed[0];
+      // retrieve properties we store with gql
       const { mime, url } = uploadDetails;
-      // update cache with personId = id
-      console.log("Update apollo cache " + id);
+      const newAvatar = { mime, url };
+      // update only said properties
+      // TODO figure out how apollo works with this
+      // client.writeQuery also performs a broadcast after writing to the cache.
+      // This broadcast ensures your data is refreshed in the view layer after the client.writeQuery operation.
+      const { profile } = client.readQuery({
+        query: GET_PROFILE,
+        variables: { id }
+      });
+      Object.assign(profile.avatar, newAvatar);
+      client.writeQuery({
+        query: GET_PROFILE,
+        variables: { id },
+        data: { profile }
+      });
+      client.queryManager.broadcastQueries();
+      refetch();
     }
   };
 
@@ -58,32 +78,36 @@ export function PeopleEditor({ profile }) {
         <div>
           Avatar:
           {avatar && avatar.url && (
-            <>
-              <img
-                alt=""
-                src={`${process.env.REACT_APP_STRAPI}/${avatar.url}`}
-                style={{
-                  maxHeight: "64px",
-                  maxWidth: "64px",
-                  height: "auto",
-                  width: "auto",
-                  cursor: "pointer"
-                }}
-                onClick={e => {
-                  e.preventDefault();
-                  document.getElementById("fileItem").click();
-                }}
-              />
-              <input
-                type="file"
-                name="files"
-                accept="image/*"
-                readOnly
-                id="fileItem"
-                onChange={_updateAvatar}
-                style={{ opacity: 0 }}
-              />
-            </>
+            <Query query={GET_PROFILE} variables={{ id }}>
+              {({ data, client, refetch }) => (
+                <>
+                  <img
+                    alt=""
+                    src={`${process.env.REACT_APP_STRAPI}/${avatar.url}`}
+                    style={{
+                      maxHeight: "64px",
+                      maxWidth: "64px",
+                      height: "auto",
+                      width: "auto",
+                      cursor: "pointer"
+                    }}
+                    onClick={e => {
+                      e.preventDefault();
+                      document.getElementById("fileItem").click();
+                    }}
+                  />
+                  <input
+                    type="file"
+                    name="files"
+                    accept="image/*"
+                    readOnly
+                    id="fileItem"
+                    onChange={_updateAvatar(data, client, refetch)}
+                    style={{ opacity: 0 }}
+                  />
+                </>
+              )}
+            </Query>
           )}
         </div>
       </div>
